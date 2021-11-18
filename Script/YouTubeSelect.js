2ffc7862a8774e5527c330bd83fde78e9421a7f1
Skyxim <skyxim@users.noreply.github.com>
2021-11-18 22:16:04 +0800

    update

--- Script/YouTubeSelect.js
+++ Script/YouTubeSelect.js
@@ -70,7 +70,7 @@ function clearCache() {
         }
     }
 }
-function selectProxy(subProxy) {
+async function selectProxy(subProxy) {
     setPolicy(youtubeGroup, subProxy)
     try {
         let region = await Promise.race([test(subProxy), timeout(3000)])
@@ -96,7 +96,7 @@ function test(nodeName) {
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
                 'Accept-Language': 'en',
             },
-            node:nodeName
+            node: nodeName
         }
         $httpClient.get(option, function (error, response, data) {
             if (error != null || response.status !== 200) {
