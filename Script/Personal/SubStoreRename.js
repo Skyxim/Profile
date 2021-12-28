c84c61ba7705c0cb87c7e32f3a53e26fa4ddd84e
Skyxim <noreply@skyxim.com>
2021-12-28 10:22:58 +0800

    update

--- Script/Personal/SubStoreRename.js
+++ Script/Personal/SubStoreRename.js
@@ -25,12 +25,12 @@ function operator(proxies) {
     }
 
     // 需要的地区 key=>匹配正则，value=>重复次数
-    let needRegion = Map()
+    let needRegion = new Map()
     needRegion.set("SG", { reg: /新加坡|SG|狮城/i, replace: 1 })
     needRegion.set("HK", { reg: /香港|HK/i, replace: 1 })
     needRegion.set("JP", { reg: /日本|JP/i, replace: 1 })
     needRegion.set("US", { reg: /美国|US/i, replace: 1 })
-    
+
     let newProxies = []
     for (const proxy of proxies) {
         let name = proxy.name
